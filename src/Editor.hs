08b304259866a4146ca30919e4ae6205c0d04696
ian <me@iany.me>
2026-02-01 13:03:18 +0800

    setup the project for editor executable

--- src/Editor.hs
+++ src/Editor.hs
@@ -9,6 +9,7 @@ import Buffer
 
 import Control.Exception
 import Control.Monad.State
+import Control.Monad (when, zipWithM_)
 
 import Control.Applicative
 import Control.Arrow       (first, second)
@@ -35,7 +36,7 @@ commands = map show [View, Edit, Next, Prev, Quit]
 -- Editor monad
 
 newtype Editor b a = Editor (StateT (b,Int) IO a)
-  deriving (Functor, Monad, MonadIO, MonadState (b,Int))
+  deriving (Functor, Applicative, Monad, MonadIO, MonadState (b,Int))
 
 runEditor :: Buffer b => Editor b a -> b -> IO a
 runEditor (Editor e) b = evalStateT e (b,0)
